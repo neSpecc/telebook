542f812f933a1862ae40679c482a68c0c46b2293
Peter Savchenko <specc.dev@gmail.com>
2023-10-01 02:08:35 +0300

    chore: button on the room screen

--- src/screens/Room.vue
+++ src/screens/Room.vue
@@ -1,9 +1,26 @@
 <script setup lang="ts">
 import WebApp from '@twa-dev/sdk'
-import { onMounted } from 'vue'
+import { onBeforeUnmount, onMounted } from 'vue'
+
+function buttonClicked() {
+  console.log('Main button clicked')
+
+  WebApp.openInvoice('https://t.me/invoice/12333', (data) => {
+    console.log('Invoice opened', data)
+  })
+  WebApp.switchInlineQuery('#133', ['users', 'groups'])
+}
 
 onMounted(() => {
-  WebApp.headerColor = '#00aaff'
+  WebApp.MainButton.text = 'Book now'
+  WebApp.MainButton.isVisible = true
+
+  WebApp.MainButton.onClick(buttonClicked)
+})
+
+onBeforeUnmount(() => {
+  WebApp.MainButton.offClick(buttonClicked)
+  WebApp.MainButton.isVisible = false
 })
 </script>
 
@@ -12,7 +29,6 @@ onMounted(() => {
     Room description
   </div>
 </template>
-
 
 <style scoped>
 
