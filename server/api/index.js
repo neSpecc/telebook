b52b2413ed8388ba11d9cab597aaaad94156a3b3
Peter Savchenko <specc.dev@gmail.com>
2023-10-09 00:19:32 +0300

    Update index.js

--- server/api/index.js
+++ server/api/index.js
@@ -3,5 +3,5 @@
 export default function handler(request, response) {
   import('../dist/index.js')
 
-  return response.send(`Hello test!`);
+  return response.send(`Hello test!` + process.env.ALLOWED_ORIGINS);
 }
