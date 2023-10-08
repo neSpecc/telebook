10d06f09954e537b8322605ccf243b6519c4a8a5
Peter Savchenko <specc.dev@gmail.com>
2023-10-09 00:27:01 +0300

    chore: test vercell ts

--- server/api/index.ts
+++ server/api/index.ts
@@ -1,4 +1,6 @@
 
-export default async function handler(request, response): Promise<any> {
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+
+export default async function handler(request: VercelRequest, response: VercelResponse): Promise<any> {
   return response.send(`Hello test!` + 'TS');
 }
