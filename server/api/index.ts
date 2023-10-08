e57f0bd3040f3d37a1c58a0d0c3eb51f15e25e51
Peter Savchenko <specc.dev@gmail.com>
2023-10-09 00:34:43 +0300

    chore: test vercel imports

--- server/api/index.ts
+++ server/api/index.ts
@@ -1,6 +1,11 @@
-
 import type { VercelRequest, VercelResponse } from '@vercel/node';
+import Config from '../src/config';
+// import HttpApi from './api/http.js';
+// import Bot from './api/bot.js';
+// import { notify } from './infra/utils/notify/index.js';
+// const bot = new Bot(Config.botToken, Config.isTestEnvironment);
+
 
 export default async function handler(request: VercelRequest, response: VercelResponse): Promise<any> {
-  return response.send(`Hello test!` + 'TS');
+  return response.send(`Hello test!` + Config.appName);
 }
