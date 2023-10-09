8ff44f7e715e4c14c57262dcfef7e7de506f2a89
Peter Savchenko <specc.dev@gmail.com>
2023-10-09 03:22:54 +0300

    Update index.ts

--- server/src/api/router/index.ts
+++ server/src/api/router/index.ts
@@ -47,9 +47,9 @@ export default async function router(fastify: FastifyInstance, opts: RouterOptio
     //   opts.bot.processUpdate(req.body as TelegramBot.Update);
     // } catch (e) {
     //   console.log('error while update processing', e);
-    }
+    // }
 
-    res.code(200);
+    res.code(200).send("OK")
   });
 
   /**
