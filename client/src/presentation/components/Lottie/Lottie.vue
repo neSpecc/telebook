b543aee5ac7471c12ada67ec5477da23933f24b1
Peter Savchenko <specc.dev@gmail.com>
2023-10-10 19:13:29 +0300

    fix(build): rm docs section from sfc

--- client/src/presentation/components/Lottie/Lottie.vue
+++ client/src/presentation/components/Lottie/Lottie.vue
@@ -9,27 +9,9 @@ defineProps<{
 </script>
 
 <template>
-   <Vue3Lottie
+  <Vue3Lottie
     :animation-data="animationData"
     :width="width"
     :height="height"
   />
 </template>
-
-<style scoped>
-
-</style>
-
-<docs>
-
-Lottie component allows you to render an animation from a JSON file. You can find more information about Lottie [here](https://airbnb.design/lottie/).
-
-## Props
-
-| Name | Type | Default | Description |
-| --- | --- | --- | --- |
-| animationData | `object` | `undefined` | The animation data. |
-| width | `string` | `undefined` | The width of the animation. |
-| height | `string` | `undefined` | The height of the animation. |
-
-</docs>
